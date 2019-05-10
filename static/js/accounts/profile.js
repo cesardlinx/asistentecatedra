c95dda05e254d529d5dfb008fcabd372e7270697
David Padilla <davidpadilla.f22@gmail.com>
2019-05-10 17:39:49 -0500

    Profile frontend for logo and photo uploading
    Now that the backend is ready to receive a user's photo and logo,
    is the moment to update the frontend so that the user can interact
    with the page and upload his images.
    
    Changes:
    - jquery-cropper was added to node modules for getting the photo
    cropping information
    - The "Comenzar ya" button in home was changed to an anchor tag
    - Custom styles for cropper.js  was added
    - Styles for supporting the logo image was added
    - Javascript code added for logo preview when user adds an image
    - Javascript for photo uploading and sending data

--- static/js/accounts/profile.js
+++ static/js/accounts/profile.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
     /**
      * Form Validation
      */
@@ -40,5 +38,97 @@ $(document).ready(function() {
             formulario.submit();
         }
     });
+
+
+    // when click on photo open the file loader
+    $('#user-photo').on('click', function() {
+        $('#id_photo').click();
+    });
+
+    // when click on photo open the file loader
+    $('#school-logo').on('click', function() {
+        $('#id_institution_logo').click();
+    });
+
+    $('#id_institution_logo').on('change', function (event) {
+
+
+        var isFile = event.target.files && event.target.files[0]
+        var helpText = '';
+
+        if (isFile) {
+
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#school-logo').attr('src', e.target.result);
+            };
+            reader.readAsDataURL(event.target.files[0]);
+
+            helpText = 'Haga click en "Guardar Cambios" para almacenar';
+        } else {
+            helpText = 'Click en el logo para cargar uno nuevo.';
+            $('#school-logo').attr('src', '/static/img/defaults/default-school-logo.png');
+        }
+
+        $('p.school-logo-help').text(helpText);
+    });
+
+    /* SCRIPT TO OPEN THE MODAL WITH THE PREVIEW */
+    $("#id_photo").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $("#image").attr("src", e.target.result);
+                $("#modalCrop").modal("show");
+            }
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+
+
+    /* SCRIPTS TO HANDLE THE CROPPER BOX */
+
+    var $image = $("#image");
+    var cropBoxData;
+    var canvasData;
+    $("#modalCrop").on("shown.bs.modal", function () {
+        $image.cropper({
+            viewMode: 1,
+            aspectRatio: 1 / 1,
+            minCropBoxWidth: 200,
+            minCropBoxHeight: 200,
+            ready: function () {
+                $image.cropper("setCanvasData", canvasData);
+                $image.cropper("setCropBoxData", cropBoxData);
+            }
+        });
+    }).on("hidden.bs.modal", function () {
+        cropBoxData = $image.cropper("getCropBoxData");
+        canvasData = $image.cropper("getCanvasData");
+        $image.cropper("destroy");
+    });
+
+    // Enable zoom in button
+    $(".js-zoom-in").click(function () {
+        $image.cropper("zoom", 0.1);
+    });
+
+    // Enable zoom out button
+    $(".js-zoom-out").click(function () {
+        $image.cropper("zoom", -0.1);
+    });
+
+    // when click on the button "recortar y cargar"
+    // fills the form, hides the modal and submits the data
+    $(".js-crop-and-upload").click(function () {
+        var cropData = $image.cropper("getData");
+        $("#id_x").val(cropData["x"]);
+        $("#id_y").val(cropData["y"]);
+        $("#id_height").val(cropData["height"]);
+        $("#id_width").val(cropData["width"]);
+        $('#modalCrop').modal('hide');
+        $('#photoForm').submit();
+    });
+
 
 });
