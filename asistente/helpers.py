81c989732463578740b72644435d8034173c2746
David Padilla <davidpadilla.f22@gmail.com>
2019-06-23 17:20:20 -0500

    Helper function for sending invoice emails
    Helper function created for sending emails to the user and the
    superusers

--- asistente/helpers.py
+++ asistente/helpers.py
@@ -33,6 +33,8 @@ def send_subscription_emails(user_mail_subject, admin_mail_subject,
         user_mail_subject, user_email_body, to=[user.email]
     )
 
+    user_email.attach_alternative(user_email_html, 'text/html')
+
     # Admins email sending
     admins = User.objects.filter(is_superuser=True, is_staff=True)
     admins_emails = [admin.email for admin in admins]
@@ -49,5 +51,32 @@ def send_subscription_emails(user_mail_subject, admin_mail_subject,
     admin_email = EmailMultiAlternatives(
         admin_mail_subject, admin_email_body, to=admins_emails
     )
+    admin_email.attach_alternative(admin_email_html, 'text/html')
 
     connection.send_messages([user_email, admin_email])
+
+
+def send_invoice_email(user_mail_subject, user, plan, amount_paid,
+                       success=True):
+    """
+    Send invoice email to user
+    """
+    # User email sending
+
+    user_email_html = render_to_string(
+        'asistente/invoice_email.html',
+        {
+            'user': user,
+            'plan': plan,
+            'amount_paid': amount_paid,
+            'success': success
+        }
+    )
+    user_email_body = strip_tags(user_email_html)
+    user_email = EmailMultiAlternatives(
+        user_mail_subject, user_email_body, to=[user.email]
+    )
+
+    user_email.attach_alternative(user_email_html, 'text/html')
+
+    user_email.send()
