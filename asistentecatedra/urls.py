696a23513147f8cf1e4cd8970b0bc177d92e8e48
David Padilla <davidpadilla.f22@gmail.com>
2019-08-03 17:46:21 -0500

    Fix error non admin user to upload images
    Fix error that a non admin user can not upload images to plannings

--- asistentecatedra/urls.py
+++ asistentecatedra/urls.py
@@ -1,15 +1,19 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import include, path
+from django.views.decorators.cache import never_cache
+from ckeditor_uploader import views as ckeditor_views
 
 urlpatterns = [
     path('', include('asistente.urls')),
     path('accounts/', include('accounts.urls')),
     path('planificaciones/', include('planificaciones.urls')),
     path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls'))
+    path('ckeditor/upload/', ckeditor_views.upload,
+         name='ckeditor_upload'),
+    path('ckeditor/browse/', never_cache(ckeditor_views.browse),
+         name='ckeditor_browse'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'asistente.views.handler404'
