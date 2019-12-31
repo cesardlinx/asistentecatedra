1f8ca1d51cc49d7137d618aa363deed34b0e8015
Cesar <davidpadilla.f22@gmail.com>
2019-12-31 13:11:16 -0500

    Superuser configuration decoupled
    Superuser configuration moved from the settings to the .env

--- asistentecatedra/settings/environments/production.py
+++ asistentecatedra/settings/environments/production.py
@@ -1,7 +1,13 @@
+from decouple import config
+
 DEBUG = False
 ALLOWED_HOSTS = ['.asistentedecatedra.com']
-ADMINS = (('Webmaster', 'davidpadilla.f22@gmail.com'),
-          ('Administrator', 'admin@asistentedecatedra.com'))
+ADMINS = (('Administrator', 'admin@asistentedecatedra.com'),)
 MANAGERS = ADMINS
 PROTOCOL = 'https'
 DOMAIN = 'asistentedecatedra.com'
+
+SUPERUSER_EMAIL = config('SUPERUSER_EMAIL')
+SUPERUSER_PASSWORD = config('SUPERUSER_PASSWORD')
+SUPERUSER_FIRSTNAME = config('SUPERUSER_FIRSTNAME')
+SUPERUSER_LASTNAME = config('SUPERUSER_LASTNAME')
