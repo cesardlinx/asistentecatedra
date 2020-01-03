5d86a1e9a8df0ff43303c44f39eafeb0d6242a24
Cesar <davidpadilla.f22@gmail.com>
2020-01-02 21:08:50 -0500

    Email settings quickfix
    The settings MAILER_EMAIL_BACKEND and DEFAULT_FROM_EMAIL were added
    to work in production

--- asistentecatedra/settings/components/email.py
+++ asistentecatedra/settings/components/email.py
@@ -2,7 +2,9 @@ from decouple import config
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+MAILER_EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_HOST = config('EMAIL_HOST')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
